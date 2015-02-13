de8050763b6cdbc35c5efb66e776d71ff045f8b2
Miguel Angel <miguela.vaqrod@gmail.com>
2015-02-13 20:24:11 +0100

    Update Extension.php

--- Extension.php
+++ Extension.php
@@ -24,11 +24,38 @@ class Extension extends \Bolt\BaseExtension
             $nemail = $this->app['request']->get($this->config['newsletter_field']);
             
             if (filter_var($nemail, FILTER_VALIDATE_EMAIL)) {
-                $html = '<script> $(document).ready(function(){ alert("'.$nemail.'"); }); </script>';
-                return new \Twig_Markup($html, 'UTF-8');                
+                         
+                $query = sprintf("SELECT id FROM %s WHERE email = '%s';", $this->config['table_name'], $nemail);
+                $id = $this->app['db']->executeQuery($query)->fetch();
+                if (!empty($id['id'])) {
+                    
+                    $html = '<script> $(document).ready(function(){ alert("ALREADY REGISTERED"); }); </script>';
+                    return new \Twig_Markup($html, 'UTF-8');
+                    
+                } else {
+                    
+                    $query = sprintf("INSERT INTO %s (slug, datecreated, datechanged, datepublish, ownerid, status, email) VALUES ('%s', '%s', '%s', '%s', 1, 'held', '%s')", 
+                    $this->config['table_name'],
+                    'slug-news' . rand(),
+                    date("Y-m-d h:m:i"),
+                    date("Y-m-d h:m:i"),
+                    date("Y-m-d h:m:i"),
+                    $nemail
+                    );
+                    if($this->app['db']->executeQuery($query)){
+                        $html = '<script> $(document).ready(function(){ alert("OK"); }); </script>';
+                        return new \Twig_Markup($html, 'UTF-8');                        
+                    }else{
+                        $html = '<script> $(document).ready(function(){ alert("DB ERROR"); }); </script>';
+                        return new \Twig_Markup($html, 'UTF-8');                        
+                    }
+                }
+                             
             }else{
+                
                 $html = '<script> $(document).ready(function(){ alert("ERROR"); }); </script>';
                 return new \Twig_Markup($html, 'UTF-8');
+                
             }
 
         }
